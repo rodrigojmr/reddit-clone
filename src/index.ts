9fae62627442b24e5119f776b52be635b20a6ef6
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-11-17 16:17:01 +0000

    Initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('start');
+import { MikroORM } from '@mikro-orm/core';
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: 'lireddit',
+    type: 'postgresql',
+    debug: process.env.NODE_ENV !== 'production'
+  });
+};
+
+main();
